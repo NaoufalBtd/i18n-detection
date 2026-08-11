50344035ef856408128681d8c29191e47e7c3080
Boutadarhart Naoufal <naoufel.info3@gmail.com>
2026-08-11 19:05:17 +0100

    fix: type expression unwrapping helpers precisely

--- src/ast-utils.ts
+++ src/ast-utils.ts
@@ -1,4 +1,4 @@
-import { Node } from 'ts-morph';
+import { Node, type ArrayLiteralExpression, type ObjectLiteralExpression } from 'ts-morph';
 
 /**
  * Removes syntax-only wrappers that do not change the runtime value of an expression.
@@ -22,13 +22,13 @@ export function unwrapExpression(node: Node): Node {
   }
 }
 
-export function asObjectLiteral(node: Node | undefined): ReturnType<Node['asKind']> | undefined {
+export function asObjectLiteral(node: Node | undefined): ObjectLiteralExpression | undefined {
   if (!node) return undefined;
   const unwrapped = unwrapExpression(node);
   return Node.isObjectLiteralExpression(unwrapped) ? unwrapped : undefined;
 }
 
-export function asArrayLiteral(node: Node | undefined): ReturnType<Node['asKind']> | undefined {
+export function asArrayLiteral(node: Node | undefined): ArrayLiteralExpression | undefined {
   if (!node) return undefined;
   const unwrapped = unwrapExpression(node);
   return Node.isArrayLiteralExpression(unwrapped) ? unwrapped : undefined;
