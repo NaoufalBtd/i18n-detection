c5450ae1930cdc6a35fc04bcd661e062315092bd
Boutadarhart Naoufal <naoufel.info3@gmail.com>
2026-08-11 19:18:11 +0100

    chore: bump scanner and report schemas to 1.2

--- src/version.ts
+++ src/version.ts
@@ -1,3 +1,3 @@
-export const TOOL_VERSION = '1.1.0';
-export const REPORT_SCHEMA_VERSION = '1.1';
-export const CACHE_SCHEMA_VERSION = '2.0';
+export const TOOL_VERSION = '1.2.0';
+export const REPORT_SCHEMA_VERSION = '1.2';
+export const CACHE_SCHEMA_VERSION = '3.0';
